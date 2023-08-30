121de60da16ad09cae85b54012aecfd1ef0428df
MUDELY SYLVIA SAMBO <91378831+mudelysambo@users.noreply.github.com>
2023-08-30 00:10:34 -1000

    openai

---
+++ betterman/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["1h3.googleusercontent.com"],
+    },
+};
+
+module.exports = nextConfig;
---
+++ betterman/src/app/create/[courseId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+const CreateChapters = (props: Props) => {
+  return (
+    <div>createChapters</div>
+  );
+}
+
+export default CreateChapters;
